38f22862d42634b6c61e8e25bd6cfb3892e9bfe8
Nobergan <nobergann@gmail.com>
2025-09-12 01:51:21 +0300

    homework 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
-import CoursesComponent from './components/homework-1/CoursesComponent.tsx';
+//   HOMEWORK 1
+// import CoursesComponent from './components/homework-1/CoursesComponent.tsx';
+import { Products } from './components/homework-2/ProductsComponent.tsx';
 
 function App() {
   return (
+    //   HOMEWORK 1
+    // <div className='min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900'>
+    //   <CoursesComponent />
+    // </div>
+
+    //   HOMEWORK 2
     <div className='min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900'>
-      <CoursesComponent />
+      <Products />
     </div>
   );
 }
